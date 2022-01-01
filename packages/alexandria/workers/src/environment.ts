ef93ddab484ae17760f2d7a1e0601e27f6248325
Nikhil Saraf <nsaraf@purestorage.com>
2022-01-01 05:02:58 -0800

    add docs related stuff

--- packages/alexandria/workers/src/environment.ts
+++
@@ -1,6 +0,0 @@
-export interface Env {
-    __STATIC_CONTENT: KVNamespace;
-//     ROOM_DURABLE_OBJECT: DurableObjectNamespace
-//     ROOM_HUB_DURABLE_OBJECT: DurableObjectNamespace
-//     RATE_LIMITER_DURABLE_OBJECT: DurableObjectNamespace
-}
